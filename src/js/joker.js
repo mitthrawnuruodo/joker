0f2f2525f26922b6dc67169494a57c807c7c6166
Lasse Hægland <seilenos@gmail.com>
2022-10-26 13:46:43 +0200

    added prettier config

--- src/js/joker.js
+++ src/js/joker.js
@@ -2,7 +2,7 @@ import joker from "give-me-a-joke";
 
 // joker.getRandomDadJoke(console.log);
 export function dadJoke() {
-    return new Promise(function(resolve, reject) {
-      joker.getRandomDadJoke(resolve);
-    })
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
 }
