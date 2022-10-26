65685fb933a91fa67ac73fe83aa5d2794b811b69
Lasse Hægland <seilenos@gmail.com>
2022-10-26 13:38:14 +0200

    export function

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,3 +1,8 @@
 import joker from "give-me-a-joke";
 
-joker.getRandomDadJoke(console.log);
+// joker.getRandomDadJoke(console.log);
+export function dadJoke() {
+    return new Promise(function(resolve, reject) {
+      joker.getRandomDadJoke(resolve);
+    })
+}
